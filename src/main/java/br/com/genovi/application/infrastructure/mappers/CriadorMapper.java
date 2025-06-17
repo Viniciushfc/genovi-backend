cb9e7fbf1c14e6cda1de706c9c54f3fa8e4cfe79
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-17 17:13:47 -0300

    feat(mapper): adicionado mapper para criador e updateEntity

--- src/main/java/br/com/genovi/application/infrastructure/mappers/CriadorMapper.java
+++ src/main/java/br/com/genovi/application/infrastructure/mappers/CriadorMapper.java
@@ -1,4 +1,35 @@
 package br.com.genovi.application.infrastructure.mappers;
 
+import br.com.genovi.application.domain.models.Criador;
+import br.com.genovi.application.dtos.CriadorDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CriadorMapper {
+
+    public Criador toEntity(CriadorDTO dto) {
+        return new Criador(
+                null,
+                dto.nome(),
+                dto.cpfCnpj(),
+                dto.endereco(),
+                dto.telefone()
+        );
+    }
+
+    public CriadorDTO toDTO(Criador entity) {
+        return new CriadorDTO(
+                entity.getNome(),
+                entity.getCpfCnpj(),
+                entity.getEndereco(),
+                entity.getTelefone()
+        );
+    }
+
+    public void updateEntityFromDTO(CriadorDTO dto, Criador entity) {
+        entity.setNome(dto.nome());
+        entity.setCpfCnpj(dto.cpfCnpj());
+        entity.setEndereco(dto.endereco());
+        entity.setTelefone(dto.telefone());
+    }
 }
