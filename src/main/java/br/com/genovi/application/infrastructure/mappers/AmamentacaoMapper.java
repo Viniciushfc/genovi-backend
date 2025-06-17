8e62150bbba4e31ec26a5dc9c8411679c4c743a8
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-17 14:55:00 -0300

    feat(mapper): mapper para tranform dto to entity vice versa e para update entity

--- src/main/java/br/com/genovi/application/infrastructure/mappers/AmamentacaoMapper.java
+++ src/main/java/br/com/genovi/application/infrastructure/mappers/AmamentacaoMapper.java
@@ -1,4 +1,40 @@
 package br.com.genovi.application.infrastructure.mappers;
 
+import br.com.genovi.application.domain.models.Amamentacao;
+import br.com.genovi.application.dtos.AmamentacaoDTO;
+import br.com.genovi.application.dtos.CreateAmamentacaoDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AmamentacaoMapper {
+
+    //Converter DTO para Entidade
+    public Amamentacao toEntity(CreateAmamentacaoDTO dto) {
+        return new Amamentacao(
+                null,
+                null, //sera setado na service
+                null, //sera setado na service
+                dto.dataInicio(),
+                dto.dataFim(),
+                dto.observacoes()
+        );
+    }
+
+    //Converter Entidade para DTO
+    public AmamentacaoDTO toDTO(Amamentacao amamentacao) {
+        return new AmamentacaoDTO(
+                amamentacao.getOvelhaMae(),
+                amamentacao.getCordeiroMamando(),
+                amamentacao.getDataInicio(),
+                amamentacao.getDataFim(),
+                amamentacao.getObservacoes()
+        );
+    }
+
+    //Atualizar uma entidade existente a partir do DTO
+    public void updateEntityFromDTO(CreateAmamentacaoDTO dto, Amamentacao entity) {
+        entity.setDataInicio(dto.dataInicio());
+        entity.setDataFim(dto.dataFim());
+        entity.setObservacoes(dto.observacoes());
+    }
 }
