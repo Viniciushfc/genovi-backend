1bf20f6f2f2fa6be3b63f975b87344aa99572f0c
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-18 16:02:04 -0300

    feat(dto): adicionar mapper para Usuario

---
+++ src/main/java/br/com/genovi/application/infrastructure/mappers/UsuarioMapper.java
@@ -0,0 +1,37 @@
+package br.com.genovi.application.infrastructure.mappers;
+
+import br.com.genovi.application.domain.models.Usuario;
+import br.com.genovi.application.dtos.UsuarioDTO;
+
+public class UsuarioMapper {
+
+    public Usuario toEntity(UsuarioDTO dto) {
+        return new Usuario(
+                null,
+                null,
+                dto.nome(),
+                dto.email(),
+                dto.senha(),
+                dto.perfil(),
+                dto.autenticacao2fa()
+        );
+    }
+
+    public UsuarioDTO toDTO(Usuario entity) {
+        return new UsuarioDTO(
+                entity.getNome(),
+                entity.getEmail(),
+                entity.getSenha(),
+                entity.getPerfil(),
+                entity.getAutenticacao2fa()
+        );
+    }
+
+    public void updateEntityFromDTO(UsuarioDTO dto, Usuario entity) {
+        entity.setNome(dto.nome());
+        entity.setEmail(dto.email());
+        entity.setSenha(dto.senha());
+        entity.setPerfil(dto.perfil());
+        entity.setAutenticacao2fa(dto.autenticacao2fa());
+    }
+}
