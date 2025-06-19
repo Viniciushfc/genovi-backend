a32b172fe090de87c5e3aead1f28899c18271ba6
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-19 08:48:25 -0300

    feat(dto): regra de negocio para doença adicionado.

---
+++ src/main/java/br/com/genovi/application/application/services/DoencaService.java
@@ -0,0 +1,50 @@
+package br.com.genovi.application.application.services;
+
+import br.com.genovi.application.domain.models.Criador;
+import br.com.genovi.application.domain.models.Doenca;
+import br.com.genovi.application.dtos.DoencaDTO;
+import br.com.genovi.application.infrastructure.mappers.DoencaMapper;
+import br.com.genovi.application.infrastructure.repositories.DoencaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DoencaService {
+
+    private final DoencaRepository doencaRepository;
+    private final DoencaMapper doencaMapper;
+
+    public DoencaService(DoencaRepository doencaRepository, DoencaMapper doencaMapper) {
+        this.doencaRepository = doencaRepository;
+        this.doencaMapper = doencaMapper;
+    }
+
+    private Doenca findDoencaEntityById(Long id) {
+        return doencaRepository.findById(id).orElseThrow(() -> new RuntimeException("Doença não encontrada"));
+    }
+
+    public List<DoencaDTO> findAll() {
+        return doencaRepository.findAll().stream().map(doencaMapper::toDto).toList();
+    }
+
+    public DoencaDTO findById(Long id) {
+        return doencaMapper.toDto(doencaRepository.findById(id).orElseThrow(() -> new RuntimeException("Doença não encontrada")));
+    }
+
+    public DoencaDTO save(DoencaDTO dto) {
+        Doenca doenca = doencaMapper.toEntity(dto);
+        return doencaMapper.toDto(doencaRepository.save(doenca));
+    }
+
+    public DoencaDTO update(Long id, DoencaDTO dto) {
+        Doenca doenca = findDoencaEntityById(id);
+        doencaMapper.updateEntityFromDto(dto, doenca);
+        return doencaMapper.toDto(doencaRepository.save(doenca));
+    }
+
+    public void delete(Long id) {
+        findDoencaEntityById(id);
+        doencaRepository.deleteById(id);
+    }
+}
