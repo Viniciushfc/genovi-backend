cdc2ac3dc73125968503833c008453fec4b983c7
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-20 11:14:56 -0300

    feat(mapper): mapper para OcorrenciaDoenca adicionados.

---
+++ src/main/java/br/com/genovi/application/infrastructure/mappers/OcorrenciaDoencaMapper.java
@@ -0,0 +1,54 @@
+package br.com.genovi.application.infrastructure.mappers;
+
+import br.com.genovi.application.domain.models.Doenca;
+import br.com.genovi.application.domain.models.OcorrenciaDoenca;
+import br.com.genovi.application.domain.models.Ovino;
+import br.com.genovi.application.domain.models.Usuario;
+import br.com.genovi.application.dtos.CreateOcorrenciaDoencaDTO;
+import br.com.genovi.application.dtos.OcorrenciaDoencaDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OcorrenciaDoencaMapper {
+
+    private final UsuarioMapper usuarioMapper;
+    private final OvinoMapper ovinoMapper;
+    private final DoencaMapper doencaMapper;
+
+    public OcorrenciaDoencaMapper(UsuarioMapper usuarioMapper, OvinoMapper ovinoMapper, DoencaMapper doencaMapper) {
+        this.usuarioMapper = usuarioMapper;
+        this.ovinoMapper = ovinoMapper;
+        this.doencaMapper = doencaMapper;
+    }
+
+    public OcorrenciaDoenca toEntity(CreateOcorrenciaDoencaDTO dto, Ovino ovino, Doenca doenca, Usuario usuario) {
+        return new OcorrenciaDoenca(
+                null,
+                ovino,
+                doenca,
+                dto.dataInicio(),
+                dto.dataFinal(),
+                dto.curado(),
+                usuario
+        );
+    }
+
+    public OcorrenciaDoencaDTO toDTO(OcorrenciaDoenca entity) {
+        return new OcorrenciaDoencaDTO(
+                ovinoMapper.toDTO(entity.getOvino()),
+                doencaMapper.toDTO(entity.getDoenca()),
+                entity.getDataInicio(),
+                entity.getDataFinal(),
+                entity.isCurada(),
+                usuarioMapper.toDTO(entity.getResponsavel()));
+    }
+
+    public void updateEntityFromDTO(CreateOcorrenciaDoencaDTO dto, OcorrenciaDoenca entity, Ovino ovino, Doenca doenca, Usuario usuario) {
+        entity.setOvino(ovino);
+        entity.setDoenca(doenca);
+        entity.setDataInicio(dto.dataInicio());
+        entity.setDataFinal(dto.dataFinal());
+        entity.setCurada(dto.curado());
+        entity.setResponsavel(usuario);
+    }
+}
