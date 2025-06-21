f7686fe7fbfc9a2343c238c4d3ef7533372d1063
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-21 15:53:01 -0300

    feat(controllers): endpoints para registro.

---
+++ src/main/java/br/com/genovi/application/application/controllers/RegistroController.java
@@ -0,0 +1,23 @@
+package br.com.genovi.application.application.controllers;
+
+import br.com.genovi.application.application.services.RegistroService;
+import br.com.genovi.application.dtos.RegistroDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/registros")
+public class RegistroController {
+
+    private final RegistroService registroService;
+
+    public RegistroController(RegistroService registroService) {
+        this.registroService = registroService;
+    }
+
+    @GetMapping("/{ovinoId}")
+    public ResponseEntity<RegistroDTO> gerarRegistroPorOvino(@PathVariable Long ovinoId) {
+        RegistroDTO registroDTO = registroService.gerarRegistroPorOvinoById(ovinoId);
+        return ResponseEntity.ok(registroDTO);
+    }
+}
