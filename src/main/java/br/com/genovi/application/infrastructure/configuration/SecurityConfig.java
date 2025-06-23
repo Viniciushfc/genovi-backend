2833ac2440a4002d6146851d5ed0a39b9a8df41a
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-23 18:19:54 -0300

    refactor(configuration): refatorando SecurityFilter para liberar apenas endpoints "publics".

--- src/main/java/br/com/genovi/application/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/br/com/genovi/application/infrastructure/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.genovi.application.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,16 +11,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //Bean apenas para facilitar os testes via requisição API
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .headers(headers -> headers
-                        .frameOptions(frame -> frame.sameOrigin())
-                )
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults()) // Login básico
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
