9b1775d1af1c356f57fdc687e440fc14b1deddf5
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-24 14:44:37 -0300

    refactor(dto): corrigindo mudanças no DTO.

--- src/main/java/br/com/genovi/application/dtos/UsuarioDTO.java
+++ src/main/java/br/com/genovi/application/dtos/UsuarioDTO.java
@@ -2,7 +2,7 @@ package br.com.genovi.application.dtos;
 
 import br.com.genovi.application.domain.enums.TypeUsuario;
 
-public record UsuarioDTO(String nome,
+public record UsuarioDTO(String username,
                          String email,
                          String senha,
                          TypeUsuario perfil,
