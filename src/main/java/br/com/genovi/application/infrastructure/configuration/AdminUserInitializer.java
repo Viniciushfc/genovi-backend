890673cb6d9d23c4907aed3b4e04c383e1adf58e
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-24 14:54:38 -0300

    feat(configuration): adição de bean para criação de usuario admin para testes.

---
+++ src/main/java/br/com/genovi/application/infrastructure/configuration/AdminUserInitializer.java
@@ -0,0 +1,30 @@
+package br.com.genovi.application.infrastructure.configuration;
+
+import br.com.genovi.application.domain.enums.Role;
+import br.com.genovi.application.domain.models.Usuario;
+import br.com.genovi.application.infrastructure.repositories.UsuarioRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserInitializer {
+
+    @Bean
+    public ApplicationRunner adminInitializer(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (usuarioRepository.findByUsername("admin").isEmpty()) {
+                Usuario admin = new Usuario();
+                admin.setUsername("admin");
+                admin.setSenha(passwordEncoder.encode("1234"));
+                admin.setRoles(Set.of(Role.ROLE_ADMIN, Role.ROLE_USER));
+
+                usuarioRepository.save(admin);
+                System.out.println("✔️ Admin user created with username 'admin' and password '1234'");
+            }
+        };
+    }
+}
