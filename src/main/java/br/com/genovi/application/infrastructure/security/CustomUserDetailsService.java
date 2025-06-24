3c181934bd045dbb936038071565ae9d39b84106
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-06-24 14:51:01 -0300

    feat(security): adição de UserDetailService Custom para a aplicação.

---
+++ src/main/java/br/com/genovi/application/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package br.com.genovi.application.infrastructure.security;
+
+import br.com.genovi.application.domain.models.Usuario;
+import br.com.genovi.application.infrastructure.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario user = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getSenha(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.name()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
