b536b03ebd414879facab5553975dc4cc3e8120d
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-07-03 16:03:51 -0300

    feat(services): regras de negocio para geração de relatorio.

---
+++ src/main/java/br/com/genovi/application/services/RelatorioService.java
@@ -0,0 +1,56 @@
+package br.com.genovi.application.services;
+
+import br.com.genovi.dtos.ovino.OvinoDTO;
+import net.sf.jasperreports.engine.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class RelatorioService {
+
+    private final OvinoService ovinoService;
+
+    public RelatorioService(OvinoService ovinoService) {
+        this.ovinoService = ovinoService;
+    }
+
+    public byte[] gerarRelatorioRegistro(Long id) throws Exception {
+        OvinoDTO ovino = ovinoService.findById(id);
+
+        //le o arquivo .jrxml
+        InputStream inputStream = new ClassPathResource("reports/relatorio-registro.jrxml").getInputStream();
+
+        //compila o arquivo para .jasper
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+
+        //parametrização para inserção no relatorio
+        Map<String, Object> parametros = new HashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        parametros.put("title", "Registro de Ovino");
+        parametros.put("nome", ovino.nome() != null ? ovino.nome() : "N/A");
+        parametros.put("fbb", ovino.fbb() != null ? ovino.fbb() : "N/A");
+        parametros.put("nascimento", ovino.dataNascimento() != null ? ovino.dataNascimento().format(formatter) : "N/A");
+        parametros.put("sexo", ovino.sexo() != null ? ovino.sexo().name() : "N/A");
+        parametros.put("rfid", ovino.rfid() != null ? String.valueOf(ovino.rfid()) : "N/A");
+        parametros.put("pureza", ovino.typeGrauPureza() != null ? ovino.typeGrauPureza().name() : "N/A");
+        parametros.put("raca", ovino.raca() != null ? ovino.raca() : "N/A");
+        parametros.put("criador_nome", ovino.criador().getNome() != null ? ovino.criador().getNome() : "N/A");
+        parametros.put("criador_cpfCnpj", ovino.criador().getCpfCnpj() != null ? ovino.criador().getCpfCnpj() : "N/A");
+        parametros.put("criador_endereco", ovino.criador().getEndereco() != null ? ovino.criador().getEndereco() : "N/A");
+        parametros.put("criador_telefone", ovino.criador().getTelefone() != null ? ovino.criador().getTelefone(): "N/A");
+
+        //Preenche o relatorio
+        JasperPrint print = JasperFillManager.fillReport(report, parametros, new JREmptyDataSource());
+
+        //exporta pra pdf
+        return JasperExportManager.exportReportToPdf(print);
+    }
+
+}
+
