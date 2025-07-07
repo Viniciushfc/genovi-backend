196bfe6c2a4ec83111e4fc6110bc74ae01a55cba
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-07-07 17:58:45 -0300

    refactor(mapper): adicionado mapper para ovino em asdcendenciaMapper para tratar o toDTO.

--- src/main/java/br/com/genovi/infrastructure/mappers/AscendenciaMapper.java
+++ src/main/java/br/com/genovi/infrastructure/mappers/AscendenciaMapper.java
@@ -4,10 +4,13 @@ import br.com.genovi.domain.models.Ascendencia;
 import br.com.genovi.domain.models.Ovino;
 import br.com.genovi.dtos.ascendencia.AscendenciaDTO;
 import br.com.genovi.dtos.ascendencia.CreateAscendenciaDTO;
+import br.com.genovi.dtos.ovino.OvinoDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AscendenciaMapper {
+
+    private OvinoMapper ovinoMapper;
 
     public Ascendencia toEntity(CreateAscendenciaDTO dto, Ovino pai, Ovino mae) {
         return new Ascendencia(
@@ -19,12 +22,12 @@ public class AscendenciaMapper {
 
     public AscendenciaDTO toDTO(Ascendencia entity) {
         return new AscendenciaDTO(
-                entity.getPai(),
-                entity.getMae()
+                ovinoMapper.toDTO(entity.getPai()),
+                ovinoMapper.toDTO(entity.getMae())
         );
     }
 
-    public void updateEntityFromDTO(Ascendencia entity, Ovino pai, Ovino mae ) {
+    public void updateEntityFromDTO(Ascendencia entity, Ovino pai, Ovino mae) {
         entity.setPai(pai);
         entity.setMae(mae);
     }
