6284fe32e2fd24f45d88d5beda4d7ba7ef50d432
ViniciusHFC <vinicius.henry.fc@gmail.com>
2025-07-09 15:39:30 -0300

    refactor(dto): correção em DTO de Exibição

--- src/main/java/br/com/genovi/dtos/amamentacao/AmamentacaoDTO.java
+++ src/main/java/br/com/genovi/dtos/amamentacao/AmamentacaoDTO.java
@@ -1,11 +1,12 @@
 package br.com.genovi.dtos.amamentacao;
 
 import br.com.genovi.domain.models.Ovino;
+import br.com.genovi.dtos.ovino.OvinoDTO;
 
 import java.time.LocalDateTime;
 
-public record AmamentacaoDTO(Ovino ovelhaMae,
-                             Ovino cordeiroMamando,
+public record AmamentacaoDTO(OvinoDTO ovelhaMae,
+                             OvinoDTO cordeiroMamando,
                              LocalDateTime dataInicio,
                              LocalDateTime dataFim,
                              String observacoes) {
--- src/main/java/br/com/genovi/dtos/reproducao/ReproducaoDTO.java
+++ src/main/java/br/com/genovi/dtos/reproducao/ReproducaoDTO.java
@@ -2,12 +2,13 @@ package br.com.genovi.dtos.reproducao;
 
 import br.com.genovi.domain.enums.TypeReproducao;
 import br.com.genovi.domain.models.Ovino;
+import br.com.genovi.dtos.ovino.OvinoDTO;
 
 import java.time.LocalDateTime;
 
 public record ReproducaoDTO(LocalDateTime dataReproducao,
-                            Ovino carneiroId,
-                            Ovino ovelhaId,
+                            OvinoDTO carneiroId,
+                            OvinoDTO ovelhaId,
                             TypeReproducao typeReproducao,
                             String observacoes) {
 }
