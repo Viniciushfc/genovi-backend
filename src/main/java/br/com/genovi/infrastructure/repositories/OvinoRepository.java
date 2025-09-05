1694b1e8031571442e5c781c74164f71dc19f6c7
viniciushfc <vinicius.henry.fc@gmail.com.br>
2025-09-05 00:00:43 -0300

    feat(repository): adicionado query para procurar por rfid

--- src/main/java/br/com/genovi/infrastructure/repositories/OvinoRepository.java
+++ src/main/java/br/com/genovi/infrastructure/repositories/OvinoRepository.java
@@ -2,10 +2,12 @@ package br.com.genovi.infrastructure.repositories;
 
 import br.com.genovi.domain.models.Ovino;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OvinoRepository extends JpaRepository<Ovino, Long> {
 
-    List<Ovino> findByRfid(Long rfid);
+    Ovino findByRfid(@Param("rfid") Long rfid);
 }
