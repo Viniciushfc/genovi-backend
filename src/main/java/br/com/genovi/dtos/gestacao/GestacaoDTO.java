d7118b5a3a950d764b14e83b72b3430dc57caad9
viniciushfc <vinicius.ferrari@gmail.com>
2025-09-30 20:12:05 -0300

    feature(dto): adicionado novo campo data de ambos.

--- src/main/java/br/com/genovi/dtos/gestacao/GestacaoDTO.java
+++ src/main/java/br/com/genovi/dtos/gestacao/GestacaoDTO.java
@@ -3,9 +3,12 @@ package br.com.genovi.dtos.gestacao;
 import br.com.genovi.dtos.ovino.OvinoDTO;
 import br.com.genovi.dtos.reproducao.ReproducaoDTO;
 
+import java.time.LocalDateTime;
+
 public record GestacaoDTO(
         Long id,
         OvinoDTO ovelhaMae,
         OvinoDTO ovelhaPai,
-        ReproducaoDTO reproducao) {
+        ReproducaoDTO reproducao,
+        LocalDateTime dataGestacao) {
 }
--- src/main/java/br/com/genovi/dtos/parto/PartoDTO.java
+++ src/main/java/br/com/genovi/dtos/parto/PartoDTO.java
@@ -3,9 +3,12 @@ package br.com.genovi.dtos.parto;
 import br.com.genovi.dtos.gestacao.GestacaoDTO;
 import br.com.genovi.dtos.ovino.OvinoDTO;
 
+import java.time.LocalDateTime;
+
 public record PartoDTO(
         Long id,
         GestacaoDTO gestacaoId,
         OvinoDTO ovelhaMaeId,
-        OvinoDTO ovelhaPaiId) {
+        OvinoDTO ovelhaPaiId,
+        LocalDateTime dataParto) {
 }
