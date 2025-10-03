fa43ebaa6cc7d3b68c39a840c639d0a13283f0cf
viniciushfc <vinicius.ferrari@gmail.com>
2025-10-03 19:32:10 -0300

    feature(DTOs): adicionado para retornar para o front MedicamentoDTO

--- src/main/java/br/com/genovi/dtos/medicamento/MedicamentoDTO.java
+++ src/main/java/br/com/genovi/dtos/medicamento/MedicamentoDTO.java
@@ -4,10 +4,12 @@ import br.com.genovi.dtos.doencas.DoencaDTO;
 
 import java.util.List;
 
-public record MedicamentoDTO(String nome,
-                             String fabricante,
-                             List<DoencaDTO> doencas,
-                             Boolean doseUnica,
-                             Integer quantidadeDoses,
-                             boolean isVacina) {
+public record MedicamentoDTO(
+        Long id,
+        String nome,
+        String fabricante,
+        List<DoencaDTO> doencas,
+        Boolean doseUnica,
+        Integer quantidadeDoses,
+        boolean isVacina) {
 }
