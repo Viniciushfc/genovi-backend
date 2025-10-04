4f1031b94e10e4e54bc85d170fcab151f2f18c20
viniciushfc <vinicius.ferrari@gmail.com>
2025-10-03 21:03:54 -0300

    fix(DTOs): correcao de teste e de entidades para novos campos adicionados

--- src/main/java/br/com/genovi/dtos/medicamento/CreateMedicamentoDTO.java
+++ src/main/java/br/com/genovi/dtos/medicamento/CreateMedicamentoDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 public record CreateMedicamentoDTO(String nome,
                                    String fabricante,
                                    List<Long> doencasIds,
+                                   Integer intervaloDoses,
                                    Integer quantidadeDoses,
                                    boolean isVacina) {
 }
