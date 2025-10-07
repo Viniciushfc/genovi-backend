47a887b4b0689202eff840a6a8c8b58ddccbaf49
viniciushfc <vinicius.ferrari@gmail.com>
2025-10-06 21:01:23 -0300

    fix(DTOs): Adição de id

--- src/main/java/br/com/genovi/dtos/aplicacao/AplicacaoDTO.java
+++ src/main/java/br/com/genovi/dtos/aplicacao/AplicacaoDTO.java
@@ -5,8 +5,10 @@ import br.com.genovi.dtos.ovino.OvinoDTO;
 
 import java.time.LocalDateTime;
 
-public record AplicacaoDTO(LocalDateTime dataAplicacao,
-                           OvinoDTO ovino,
-                           MedicamentoDTO medicamento,
-                           LocalDateTime dataProximaDose) {
+public record AplicacaoDTO(
+        Long id,
+        LocalDateTime dataAplicacao,
+        OvinoDTO ovino,
+        MedicamentoDTO medicamento,
+        LocalDateTime dataProximaDose) {
 }
