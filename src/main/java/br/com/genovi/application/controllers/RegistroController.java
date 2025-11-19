c8aa63cb66ad6495983aa2eae339788bced079db
viniciushfc <vinicius.ferrari@gmail.com>
2025-11-19 20:42:21 -0300

    fix: correção no endpoint de sugestão

--- src/main/java/br/com/genovi/application/controllers/RegistroController.java
+++ src/main/java/br/com/genovi/application/controllers/RegistroController.java
@@ -24,6 +24,6 @@ public interface RegistroController {
     @DeleteMapping("/{id}")
     ResponseEntity<Void> delete(@PathVariable Long id);
 
-    @PatchMapping("/sugestao")
+    @PatchMapping("/{id}/sugestao")
     ResponseEntity<RegistroDTO> sugestaoParaRegistro(@PathVariable Long id, @RequestBody Boolean isSugestao);
 }
